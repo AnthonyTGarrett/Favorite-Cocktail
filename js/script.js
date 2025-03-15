f0bd9edf85f47c251222adccca3787a6499d2170
Tony Garrett <tony.garrett@gmail.com>
2025-03-14 23:36:44 -0400

    Making great progress

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, { edge: 'right' });
-
-  var elems = document.querySelectorAll('.autocomplete');
-  var instances = M.Autocomplete.init(elems, {
-    data: {
-      Google: null,
-      Apple: null,
-      Microsoft: null,
-    },
-    limit: 2,
-  });
 });
