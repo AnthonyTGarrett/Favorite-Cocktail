a6728e9da5b15f54d238ee17bc0d4b03f77425ef
Tony Garrett <tony.garrett@gmail.com>
2025-03-15 17:03:04 -0400

    baby steps

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, { edge: 'right' });
+
+  async function getData() {
+    const url = 'www.thecocktaildb.com/api/json/v1/1/random.php';
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
 });
