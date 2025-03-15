5c0f7517836667f5267787d438604895191d79d8
Tony Garrett <tony.garrett@gmail.com>
2025-03-14 21:55:07 -0400

    Working on input field autocomplete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems, { edge: 'right' });
+
+  var elems = document.querySelectorAll('.autocomplete');
+  var instances = M.Autocomplete.init(elems, {
+    data: {
+      Google: null,
+      Apple: null,
+      Microsoft: null,
+    },
+    limit: 2,
+  });
 });
