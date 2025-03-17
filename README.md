81ac42ad6721990b54efc7ddcae31a25e7cbe64c
Tony Garrett <tony.garrett@gmail.com>
2025-03-17 17:19:43 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Cocktail Recipes
 
 # Testing out Materialize CSS
+
+# This project is a way to test working with APIs
+
+# I have a more complex site in mind and wanted to practice my CSS and basic javascript
--- js/script.js
+++ js/script.js
@@ -4,13 +4,17 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 const randomButton = document.querySelector('#random-drink');
+const popularDrinksInsert = document.getElementById('popular-drink-items');
 
-randomButton.addEventListener('click', randomDisplay);
+// randomButton.addEventListener('click', randomDisplay);
 
 const singleRandomItemUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/random.php';
 const nameSearchUrl =
   'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+const popularDrinksUrl =
+  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
 
 async function fetchProducts(url) {
   const response = await fetch(url);
@@ -21,18 +25,49 @@ async function fetchProducts(url) {
   return data;
 }
 
-function randomDisplay() {
-  const promise = fetchProducts(singleRandomItemUrl);
-  promise
-    .then(data => {
-      console.log(data.drinks[0]);
-      document.querySelector('#items').innerHTML = '<h1>Trial</h1>';
-    })
-    .catch(error => {
-      console.error(`Could not get products: ${error}`);
-    })
-    .finally(() => {
-      // Navigate to the new page after the API call (success or failure)
-      window.location.href = 'drink.html';
+const popularDrinks = fetchProducts(popularDrinksUrl);
+
+popularDrinks
+  .then(data => {
+    data.drinks.forEach(element => {
+      popularDrinksInsert.innerHTML += `
+      <div class="col s12 m6 l3">
+        <div class="card">
+            <div class="card-image">
+              <img src="${element.strDrinkThumb}" />
+              <span class="card-title">${element.strDrink}</span>
+                </div>
+                <div class="card-content">
+                  <p>
+                    I am a very simple card. I am good at containing small bits
+                    of information. I am convenient because I require little
+                    markup to use effectively.
+                  </p>
+                </div>
+                <div class="card-action">
+                  <a href="#">This is a link</a>
+                </div>
+          </div>
+        </div>
+      `;
     });
-}
+  })
+  .catch(error => {
+    console.error(`Could not get products: ${error}`);
+  });
+
+// function randomDisplay() {
+//   const promise = fetchProducts(singleRandomItemUrl);
+//   promise
+//     .then(data => {
+//       console.log(data.drinks[0]);
+//       document.querySelector('#items').innerHTML = '<h1>Trial</h1>';
+//     })
+//     .catch(error => {
+//       console.error(`Could not get products: ${error}`);
+//     })
+//     .finally(() => {
+//       // Navigate to the new page after the API call (success or failure)
+//       window.location.href = 'drink.html';
+//     });
+// }
