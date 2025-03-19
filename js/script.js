dcea8ff461b989cd62eae496a15f6620e14aef3d
Anthony Garrett <tony.garrett@gmail.com>
2025-03-19 10:45:39 -0400

    Adding javascript to make the single specific drink work

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const randomButton = document.getElementById('random-drink');
 const popularDrinksInsert = document.getElementById('popular-drink-items');
+const singleDrinkButton = document.querySelector('.single-drink-btn');
 const singleDrinkInsert = document.querySelector('.single-drink-insert');
+const userDrinkInput = document.getElementById('drink');
 
 // randomButton.addEventListener('click', randomDisplay);
+// singleDrinkButton.addEventListener('click', displaySingleDrink);
 
 const singleRandomItemUrl =
   'https://www.thecocktaildb.com/api/json/v2/961249867/random.php';
@@ -59,34 +62,35 @@ window.onload = event => {
         }
       }
     });
+  } else if (document.URL.includes('drink')) {
   } else {
     popularDrinks
       .then(data => {
         const partialArr = data.drinks.splice(0, 9);
         partialArr.forEach(element => {
           popularDrinksInsert.innerHTML += `
-      <div class="col s12 m6 l4">
-        <div class="card hoverable">
-            <div class="card-image">
-              <img src="${element.strDrinkThumb}" />
-              <span class="card-title">${element.strDrink}</span>
-                </div>
-                <div class="card-content">
-                  <p>
-                    Glass: ${element.strGlass}
-                  </p>
-                  <p>
-                    Alcohol: ${element.strIngredient1}
-                  </p>
-                  <p>
-                    Type: ${element.strAlcoholic}
-                  </p>
-                </div>
-                <div class="card-action">
-                  <a href="#">Full Recipe</a>
-                </div>
-          </div>
-        </div>
+          <div class="col s12 m6 l4">
+            <div class="card hoverable">
+                <div class="card-image">
+                  <img src="${element.strDrinkThumb}" />
+                  <span class="card-title">${element.strDrink}</span>
+                    </div>
+                    <div class="card-content">
+                      <p>
+                        Glass: ${element.strGlass}
+                      </p>
+                      <p>
+                        Alcohol: ${element.strIngredient1}
+                      </p>
+                      <p>
+                        Type: ${element.strAlcoholic}
+                      </p>
+                    </div>
+                    <div class="card-action">
+                      <a href="#">Full Recipe</a>
+                    </div>
+              </div>
+            </div>
       `;
         });
       })
@@ -96,19 +100,20 @@ window.onload = event => {
   }
 };
 
-// function randomDisplay() {
-//   const promise = fetchProducts(singleRandomItemUrl);
-//   promise
-//     .then(data => {
-//       // console.log(data.drinks[0]);
-//       // singleDrinkInsert.innerHTML += data.drinks[0].strDrink;
-//       // localStorage.setItem('randomDrink', data.drinks[0]);
-//     })
-//     .catch(error => {
-//       console.error(`Could not get products: ${error}`);
-//     })
-//     .finally(() => {
-//       // Navigate to the new page after the API call (success or failure)
-//       // window.location.href = 'random.html';
-//     });
-// }
+function displaySingleDrink() {
+  const userInput = userDrinkInput.value.trim().split(' ').join('&');
+
+  // const promise = fetchProducts(nameSearchUrl + userInput);
+  promise
+    .then(data => {
+      // singleDrinkInsert.innerHTML += data.drinks[0].strDrink;
+      // localStorage.setItem('randomDrink', data.drinks[0]);
+    })
+    .catch(error => {
+      console.error(`Could not get products: ${error}`);
+    })
+    .finally(() => {
+      // Navigate to the new page after the API call (success or failure)
+      window.location.href = `drink.html?${userInput}`;
+    });
+}
